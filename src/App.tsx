bd5ed24c4c435bd1050f2922c1cbc31284f45f88
Zoltan Pal <palzoltanet@gmail.com>
2025-10-13 21:07:41 +0200

    add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import Loading from "@/pages/Loading";
 import Layout from "@/components/layout";
-import WordCoOccurences from "./pages/WordCoOccurences";
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export function usePageTracking() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
+  }, [location]);
+}
+
 
 /* Static pages */
 const NotFound = lazy(() => import("@/pages/static/NotFound"));
@@ -17,11 +28,14 @@ const Trends = lazy(() => import("@/pages/Trends"));
 const CorrelationBetweenSources = lazy(() => import("@/pages/CorrelationBetweenSources"));
 const BiasDetection = lazy(() => import("@/pages/BiasDetection"));
 const RawDataViewer = lazy(() => import("@/pages/RawDataViewer"));
+const WordCoOccurences = lazy(() => import("@/pages/WordCoOccurences"));
 const LiveAnalysisKeyword = lazy(() => import("@/pages/LiveAnalysisKeyword"));
 const LiveAnalysisRss = lazy(() => import("@/pages/LiveAnalysisRss"));
 
 
+
 export default function App() {
+  usePageTracking();
   return (
     <Suspense fallback={<Loading />}>
       <Routes>
